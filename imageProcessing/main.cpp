e29b2882d06a90c74b2e61953b92c81804a36128
wubo1990 <bowu@ucdavis.edu>
2014-11-14 15:56:56 -0800

    Initial Commit

---
+++ imageProcessing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  imageProcessing
+//
+//  Created by Bo Wu on 11/14/14.
+//  Copyright (c) 2014 Bo Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
